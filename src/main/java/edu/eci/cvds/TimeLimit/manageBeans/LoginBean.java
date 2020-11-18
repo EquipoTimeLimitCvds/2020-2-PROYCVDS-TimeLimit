9f3c6ebfe80283075fa4b75c6e1f3e73d0af8bb3
Santiago Laiton <santiago.laiton@mail.escuelaing.edu.co>
2020-11-18 00:23:31 -0500

    Arreglo de contraseña, arreglo de integridad y front

--- src/main/java/edu/eci/cvds/TimeLimit/manageBeans/LoginBean.java
+++ src/main/java/edu/eci/cvds/TimeLimit/manageBeans/LoginBean.java
@@ -79,4 +79,5 @@ public class LoginBean {
         FacesContext facesContext = FacesContext.getCurrentInstance();
         facesContext.getExternalContext().redirect("");
     }
+
 }
--- tables.sql
+++ tables.sql
@@ -3,7 +3,7 @@ create table if not exists "PERSONAL" (
 	nombre varchar(30) NOT NULL,
 	cargo Varchar(15) NOT NULL,
 	apellido Varchar(30) NOT NULL,
-	clave Varchar(50) NOT NULL	
+	clave Varchar(200) NOT NULL
 );
 
 CREATE SEQUENCE NoLab INCREMENT BY 1;
@@ -54,6 +54,11 @@ alter table "equipos" add constraint PK_Equipos primary key(id);
 alter table "elementos" add constraint PK_Elementos primary key(id);
 alter table "novedades" add constraint PK_Novedades primary key(id);
 
+alter table "PERSONAL" add constraint UK_name unique(nombre);
+alter table "equipos" add constraint UK_nombreEquipo unique(nombre);
+
+ALTER TABLE "equipos" ADD CONSTRAINT equipos_nombre CHECK (equipos.nombre LIKE 'Sistemas%' or equipos.nombre LIKE 'sistemas%' or equipos.nombre LIKE 'SISTEMAS%');
+
 alter table "Tiene" add constraint FK_Tiene_Personal foreign key(IdPersonal) references "PERSONAL"(id);
 alter table "Tiene" add constraint FK_Tiene_Laboratorios foreign key(IdLaboratorio) references "Laboratorios"(id);
 alter table "equipos" add constraint FK_Equipos_Laboratorios foreign key(IdLaboratorio) references "Laboratorios"(id);
@@ -63,4 +68,7 @@ alter table "novedades" add constraint FK_novedades_Laboratorios foreign key(IdE
 alter table "novedades" add constraint FK_novedades_elementos foreign key(IdElemento) references "elementos"(id);
 
 
-insert into "PERSONAL"(id,nombre,cargo,apellido,clave) values(2145058,'Steven','Monitor','Bermudez','123456');
+insert into "PERSONAL"(id,nombre,name,cargo,apellido,clave) values(2146684,'Lina10','Lina','Monitor','Buitrago','b24f3a9ad4c9fedcadf95801740ed240667b9b0ecb4b14be25616f5681341836');
+insert into "PERSONAL"(id,nombre,name,cargo,apellido,clave) values(2145058,'Steven15','Lina','Monitor','Bermudez','8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92');
+insert into "PERSONAL"(id,nombre,name,cargo,apellido,clave) values(2145059,'Santiago27','Lina','Monitor','Laiton','88f4380b893c15a3e8361a8910fb7989b4261f303643858a6963b59f507076a1');
+insert into "PERSONAL"(id,nombre,name,cargo,apellido,clave) values(2145054,'SebastianVilla','Sebastian','Monitor','Villamarin','8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92');
